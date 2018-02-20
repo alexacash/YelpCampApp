0f99784441dc34e20c5dacbe016997dd9de65b0b
Alexa Cash <alexancash@gmail.com>
2018-02-20 20:32:37 +0000

    pushing YelpCamp to Github

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index")
  
 var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v10";
-mongoose.connect(url);
-
+mongoose.connect("mongodb://alexa:Tugun2013!@ds117913.mlab.com:17913/alexacash_yc"
+);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
